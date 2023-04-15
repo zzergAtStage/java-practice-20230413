213cbcb4ee1778215483bdba3a04b543a06b7799
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-04-16 00:35:48 +0200

    exercise05 with if statements and reversal movement

--- src/main/java/com/gbjava/seminar01/HomeWorkS01.java
+++ src/main/java/com/gbjava/seminar01/HomeWorkS01.java
@@ -28,11 +28,18 @@ public class HomeWorkS01 {
         exercise04();
 
         /*
-        *  В консоли запросить имя пользователя.
-        * В зависимости от текущего времени, вывести приветствие вида:
-        * */
+         *  В консоли запросить имя пользователя.
+         * В зависимости от текущего времени, вывести приветствие вида:
+         * */
         Library.ShirkRow();
         exercise04_4();
+        /*
+         * 5.** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным,
+         * или отрицательным), при этом метод должен сместить все элементы массива на n позиций. Элементы смещаются циклично.
+         * */
+        Library.ShirkRow();
+        int arraySize = 5;
+        exercise05(arraySize);
 
     }
 
@@ -74,10 +81,11 @@ public class HomeWorkS01 {
         int stepBackDirection = arrayLength - 1;
         for (int i = 0; i < arrayLength; i++) {
             array[i][i] = 1;
-            array[stepBackDirection-i][i] = 1;
+            array[stepBackDirection - i][i] = 1;
         }
         Library.OutPutSquareArray(array);
     }
+
     private static void exercise04_4() {
         Scanner scanner = new Scanner(System.in);
         String userName;
@@ -85,7 +93,7 @@ public class HomeWorkS01 {
         final int MORNING = 5;
         final int NOON = 12;
         final int EVENING = 18;
-        final int NIGHT = 18;
+        final int NIGHT = 23;
 
 
         boolean canBeRead = scanner.hasNext();
@@ -99,16 +107,78 @@ public class HomeWorkS01 {
 
         String prompt;
         if (MORNING < currentHour && currentHour < NOON) {
-            prompt =  MessageFormat.format(promptBlueprint, "ое", "утро",userName);
+            prompt = MessageFormat.format(promptBlueprint, "ое", "утро", userName);
         } else if (NOON <= currentHour && currentHour < EVENING) {
-            prompt =  MessageFormat.format(promptBlueprint, "ый", "день",userName);
+            prompt = MessageFormat.format(promptBlueprint, "ый", "день", userName);
         } else if (EVENING <= currentHour && currentHour < NIGHT) {
-            prompt =  MessageFormat.format(promptBlueprint, "ый", "вечер",userName);
-        } else if (NIGHT <= currentHour && currentHour < MORNING) {
-            prompt =  MessageFormat.format(promptBlueprint, "ой", "ночи",userName);
+            prompt = MessageFormat.format(promptBlueprint, "ый", "вечер", userName);
+        } else if (NIGHT == currentHour || currentHour < MORNING) {
+            prompt = MessageFormat.format(promptBlueprint, "ой", "ночи", userName);
         } else {
-            prompt =  MessageFormat.format(promptBlueprint, "ого", "времени суток", userName);
+            prompt = MessageFormat.format(promptBlueprint, "ого", "времени суток", userName);
         }
         System.out.println(prompt);
     }
+
+    private static void exercise05(int arraySize) {
+        Scanner scanner = new Scanner(System.in);
+        int shiftN = 0;
+        while (shiftN == 0) {
+            System.out.print("Input shift value: ");
+            shiftN = scanner.nextInt();
+        }
+        int leftRange = 10;
+        int rightRange = 20;
+        int[] array = Library.GetArray(arraySize, leftRange, rightRange);//new int[] { 3, 5, 6, 1}
+        System.out.println("Original array: " + Arrays.toString(array));
+        ShiftArrayByNumElements(array, shiftN);// magic!
+        System.out.println("Array after sort " + Arrays.toString(array));
+    }
+
+    private static void ShiftArrayByNumElements(int[] array, int shiftN) {
+        /*         v
+        *     [ 3, 5, 6, 1]
+         *            ^  l
+        *     [ 1, 3, 5, 6]
+        *       l
+        * */
+
+        int n = array.length;
+
+        if (shiftN > 0) { // Right shift
+            for (int i = 0; i < shiftN; i++) {
+                int j,last;
+                last = array[n-1];
+                for (j = n - 1; j > 0; j--) {
+                    array[j] = array[j-1];
+                }
+                array[0] = last;
+            }
+
+        } else if (shiftN < 0) { // Left shift
+            /*       v
+             *     [ 3, 5, 6, 1]
+             *       l  ^  <-
+             *     [ 5, 6, 1, 3]
+             *                l
+             * */
+            for (int i = 0; i < Math.abs(shiftN); i++) {
+                int j,first;
+                first = array[0];
+                for (j = 0; j < n - 1; j++){
+                    array[j] = array[j+1];
+                }
+                array[n-1] = first;
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
 }
