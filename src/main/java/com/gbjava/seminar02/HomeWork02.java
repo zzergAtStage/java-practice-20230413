a4f23e7323dc5dfa73f6bf40765d0325c7468793
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-04-20 02:38:31 +0200

    task001 with sql

--- src/main/java/com/gbjava/seminar02/HomeWork02.java
+++ src/main/java/com/gbjava/seminar02/HomeWork02.java
@@ -1,4 +1,39 @@
 package com.gbjava.seminar02;
 
+
+import org.json.JSONObject;
+
+import java.util.Objects;
+
 public class HomeWork02 {
+    public static void main(String[] args) {
+
+        taskHW01();
+
+    }
+
+    private static void taskHW01() {
+        //String jsonString = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":null}";
+        String jsonString = "{\"name\":null, \"country\":null, \"city\":null, \"age\":null}";
+
+        JSONObject json = new JSONObject(jsonString);
+        String tableName = "students";
+        //build string of query
+        String resultQuery = String.valueOf(UpdateQuery(json, tableName));
+        //some output
+        System.out.println(resultQuery);
+    }
+
+    private static StringBuilder UpdateQuery(JSONObject json, String tableName) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("select * from %1$s where", tableName)).append(" 1=1");//add space before conditions!
+        for (String key : json.keySet()) {
+            String value = json.get(key).toString();
+            if ((!Objects.equals(value, "null"))) {
+                sb.append(" and ").append(key).append(" = '").append(value).append("'");
+            }
+            //if code will not be used in querying, we can add .append('\n')
+        }
+        return sb.append(';');
+    }
 }
