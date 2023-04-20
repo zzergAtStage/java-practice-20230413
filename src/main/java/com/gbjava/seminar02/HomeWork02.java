0e2fae6cd5445b4d358dcef3a9abcd5a072430c1
Sergey V. Brusentsov <brusentsovsv@gmail.com>
2023-04-20 09:53:43 +0200

    task002 with .gitignore processing

--- src/main/java/com/gbjava/seminar02/HomeWork02.java
+++ src/main/java/com/gbjava/seminar02/HomeWork02.java
@@ -3,13 +3,15 @@ package com.gbjava.seminar02;
 
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.Objects;
+import java.util.Optional;
 
 public class HomeWork02 {
     public static void main(String[] args) {
 
         taskHW01();
-
+        taskHW02();
     }
 
     private static void taskHW01() {
@@ -31,9 +33,30 @@ public class HomeWork02 {
             String value = json.get(key).toString();
             if ((!Objects.equals(value, "null"))) {
                 sb.append(" and ").append(key).append(" = '").append(value).append("'");
+                //if code will not be used in querying, we can add .append('\n')
             }
-            //if code will not be used in querying, we can add .append('\n')
         }
         return sb.append(';');
+    }
+
+    private static void taskHW02() {
+        String pathProject = System.getProperty("user.dir");
+        File f1 = new File(pathProject);
+        StringBuilder dirContent = new StringBuilder();
+        for (File object: f1.listFiles()) {//I've ignored try catch block, while it is not learned now
+            if (object.isFile()){
+                Optional<String> extName = GetFileExtension(object.getName());
+                dirContent.append(
+                        ((!extName.isEmpty()) ? ("Расширение файла: ").concat(extName.get()) : "")
+                        ).append(System.lineSeparator());
+            }
+        }
+        System.out.println(dirContent);
+
+    }
+    static Optional<String> GetFileExtension(String fileName){
+        return Optional.ofNullable(fileName)
+                .filter(f -> f.contains(".") && !f.startsWith ("."))//filters map results not nullable
+                .map(f -> f.substring(fileName.lastIndexOf(".") + 1));//works as reverse array iteration
     }
 }
