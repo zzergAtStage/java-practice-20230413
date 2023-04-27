9caedb0530d1076f1b38f33dc0abdb015c9ab416
Sergio Brü <brusentsovsv@gmail.com>
2023-04-27 19:42:29 +0200

    Seminar04 homework

--- src/main/java/com/gbjava/seminar04/HomeWork04.java
+++ src/main/java/com/gbjava/seminar04/HomeWork04.java
@@ -5,36 +5,37 @@ import java.util.Scanner;
 
 public class HomeWork04 {
     public static void main(String[] args) {
-        ex01();
+        //ex01();
+        ex02();
     }
+
     static void ex01(){
         Scanner scanner = new Scanner(System.in);
         LinkedList<String> listOfWords= new LinkedList<>();
         while (true) {
+            System.out.print("Enter new phrase (or command): ");
             String userInput = scanner.nextLine();
 
-
-                if (userInput.equalsIgnoreCase("stop")){
-                    break;
-                }
-                else if (userInput.equalsIgnoreCase("print")){
-                    PrintArrayReversal(listOfWords);
-                } else if (userInput.equalsIgnoreCase("revert")){
-                    RevertLastWord(listOfWords);
-                } else {
-                    listOfWords.add(userInput);
-                }
+            if (userInput.equalsIgnoreCase("stop")){
+                break;
+            } else if (userInput.equalsIgnoreCase("print")){
+                printArrayReversal(listOfWords);
+            } else if (userInput.equalsIgnoreCase("revert")){
+                revertLastWord(listOfWords);
+            } else {
+                listOfWords.add(userInput);
+            }
         }
 
     }
 
-    private static void RevertLastWord(LinkedList<String> listOfWords) {
+    private static void revertLastWord(LinkedList<String> listOfWords) {
         System.out.printf("Word: [%s] will be deleted from list!%n",listOfWords.getLast());
         String removed = listOfWords.removeLast();
         //listOfWords.remove(listOfWords.indexOf(listOfWords.getLast()));
     }
 
-    private static void PrintArrayReversal(LinkedList<String> listOfWords) {
+    private static void printArrayReversal(LinkedList<String> listOfWords) {
         for (int i = listOfWords.size()-1; i >= 0 ; i--) {
             try {
                 System.out.println(listOfWords.get(i));
@@ -42,5 +43,12 @@ public class HomeWork04 {
                 System.out.println("Something goes wrong: " + e.getMessage());
             }
         }
+    }
+
+    private static void ex02() {
+        LinkedList<Integer> listNums = new LinkedList<>();
+        Integer num = 5;
+        listNums.set(2, num);//I can not set elements without previous
+        System.out.println(listNums.getLast());
     }
 }
